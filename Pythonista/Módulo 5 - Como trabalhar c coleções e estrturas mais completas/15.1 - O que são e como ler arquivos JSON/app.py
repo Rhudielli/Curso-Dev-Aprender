241eabaa4bdc7fe789b9d447a273f0c5ef1f4481
Rhudielli <isaquerhudielle321@gmail.com>
2023-04-15 15:52:21 -0300

    Update app.py

--- Pythonista/Módulo 5 - Como trabalhar c coleções e estrturas mais completas/15.1 - O que são e como ler arquivos JSON/app.py
+++ Pythonista/Módulo 5 - Como trabalhar c coleções e estrturas mais completas/15.1 - O que são e como ler arquivos JSON/app.py
@@ -1,9 +1,22 @@
 import json
 
-with open('usuarios1.json', encoding='utf-8') as arquivo_json:
+with open('usuarios1.json',encoding='utf-8') as arquivo_json:
     data = json.load(arquivo_json) # Converter arquivo JSON -> String
-    print(data["nome"])
+    print(data['nome'])
 
-with open('usuarios2.json', encoding='utf-8') as arquivo2_json:
-    data = json.load(arquivo2_json)
-    print(data["permissão"][1])
+with open('usuarios2.json', encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json)
+    print(data["permissões"][1])
+
+with open('usuarios3.json',encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json)
+    print(data["usuários"][0]["telefone"])
+    print(data["usuários"][1]["admin"])
+
+with open('usuarios4.json',encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json)
+    print(data["usuários"][0]["plano"]["preco"])
+
+with open('usuarios5.json',encoding='utf-8') as arquivo_json:
+    data = json.load(arquivo_json)
+    print(data[0]["admin"])
